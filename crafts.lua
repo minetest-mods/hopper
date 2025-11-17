bf8fffa474e501d29f55bb6151a9fb58672c030a
SmallJoker <mk939@ymail.com>
2025-11-17 19:22:56 +0100

    Make hoppers craftable for mcl

--- crafts.lua
+++ crafts.lua
@@ -1,48 +1,71 @@
+local item_steel_ingot
+local item_chest
+local item_mese_crystal
+
+
+if core.get_modpath("default") then
+	item_steel_ingot  = "default:steel_ingot"
+	item_chest        = "default:chest"
+	item_mese_crystal = "default:mese_crystal"
+end
+
+-- VoxeLibre, Mineclonia
+if core.get_modpath("mcl_core") then
+	item_steel_ingot = "mcl_core:iron_ingot"
+	item_chest = "mcl_chests:chest"
+	item_mese_crystal = "mcl_ocean:prismarine_crystals"
+end
+
+if not item_mese_crystal then
+	core.log("warning", "[hopper] Unsupported game! There will be no craftable nodes.")
+	return
+end
 
 if minetest.get_modpath("default") then
 	minetest.register_craft({
 		output = "hopper:hopper",
 		recipe = {
-			{"default:steel_ingot","default:chest","default:steel_ingot"},
-			{"","default:steel_ingot",""},
+			{item_steel_ingot, item_chest, item_steel_ingot},
+			{"", item_steel_ingot, ""},
 		}
 	})
 
 	minetest.register_craft({
 		output = "hopper:chute",
 		recipe = {
-			{"default:steel_ingot","default:chest","default:steel_ingot"},
+			{item_steel_ingot, item_chest, item_steel_ingot},
 		}
 	})
 
 	minetest.register_craft({
 		output = "hopper:sorter",
 		recipe = {
-			{"","default:mese_crystal",""},
-			{"default:steel_ingot","default:chest","default:steel_ingot"},
-			{"","default:steel_ingot",""},
+			{"", item_mese_crystal, ""},
+			{item_steel_ingot, item_chest, item_steel_ingot},
+			{"", item_steel_ingot ,""},
 		}
 	})
 
-	if not hopper.config.single_craftable_item then
-		minetest.register_craft({
-			output = "hopper:hopper_side",
-			recipe = {
-				{"default:steel_ingot","default:chest","default:steel_ingot"},
-				{"","","default:steel_ingot"},
-			}
-		})
+end
 
-		minetest.register_craft({
-			output = "hopper:hopper_side",
-			type="shapeless",
-			recipe = {"hopper:hopper"},
-		})
+if not hopper.config.single_craftable_item then
+	minetest.register_craft({
+		output = "hopper:hopper_side",
+		recipe = {
+			{item_steel_ingot, item_chest, item_steel_ingot},
+			{"", "", item_steel_ingot},
+		}
+	})
 
-		minetest.register_craft({
-			output = "hopper:hopper",
-			type="shapeless",
-			recipe = {"hopper:hopper_side"},
-		})
-	end
+	minetest.register_craft({
+		output = "hopper:hopper_side",
+		type = "shapeless",
+		recipe = {"hopper:hopper"},
+	})
+
+	minetest.register_craft({
+		output = "hopper:hopper",
+		type = "shapeless",
+		recipe = {"hopper:hopper_side"},
+	})
 end
